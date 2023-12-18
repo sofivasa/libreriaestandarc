f7f9863bd1a02375927e4028f1e9075190522fd3
sofivasa <sofivasa@myuax.com>
2023-12-18 22:45:21 +0100

    update2

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include <string.h>
-#include <stdio.h> int main() {
+#include <stdio.h>
+int main() {
  char str1[] = "Hola";
 char str2[] = "Mundo";
 int comparison = strcmp(str1, str2);
-(if (comparison == 0) ){ printf("Las cadenas son iguales.\n");
+if (comparison == 0) { printf("Las cadenas son iguales.\n");
 }
-(else { printf("Las cadenas son diferentes.\n");
+else { printf("Las cadenas son diferentes.\n");
 }
 return 0;
+}
