1b5ab7bc9369e6ff665c9fde0eb607219c86990d
sofivasa <sofivasa@myuax.com>
2023-12-18 23:01:14 +0100

    comentarios

--- main.c
+++ main.c
@@ -1,10 +1,17 @@
 #include <string.h>
 #include <stdio.h>
 int main() {
+
+    //Crear dos cadenas diferentes
  char str1[] = "Hola";
 char str2[] = "Mundo";
+
+//Usar la función strcmp para comparar las cadenas
 int comparison = strcmp(str1, str2);
-if (comparison == 0) { printf("Las cadenas son iguales.\n");
+if (comparison == 0)
+
+//Imprimir el resultado de la comparación
+{ printf("Las cadenas son iguales.\n");
 }
 else { printf("Las cadenas son diferentes.\n");
 }
